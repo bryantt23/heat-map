fa401b50a17af5040b095e2bee25f6ac2bfce030
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-05-03 19:07:14 -0700

    get data

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('hi');
+let dataset;
+const padding = 60;
+let w = 500,
+  h = 500,
+  xScale = 1,
+  yScale = 1,
+  startDate = 10000,
+  endDate = 0,
+  startTime = new Date(),
+  endTime = new Date(null),
+  rangeOfYears,
+  rangeOfTime;
+
+const timeFormat = d3.timeFormat('%M:%S');
+const keys = ['No doping allegations', 'Riders with doping allegations'];
+const size = 20;
+
+async function getData() {
+  const resp = await axios.get(
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
+  );
+  dataset = resp.data;
+  console.log(dataset);
+}
+
+getData();
