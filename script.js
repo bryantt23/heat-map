117440f762db7655e08c3bc95c316f2f36b1a262
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-05-03 21:16:08 -0700

    show months via https://stackoverflow.com/questions/40173533/customize-the-d3-month-or-year-tick-format

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const w = 500,
+const w = 1000,
   h = 500,
   padding = 60,
   rectHeight = h / 12;
@@ -18,12 +18,20 @@ async function getData() {
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
   );
   dataset = resp.data;
+  console.log(dataset);
+  dataset.monthlyVariance = [
+    ...dataset.monthlyVariance.slice(0, 10),
+    ...dataset.monthlyVariance.slice(3000)
+  ];
+  console.log(dataset);
   baseTemperature = dataset.baseTemperature;
   console.log(dataset);
   startYear = d3.min(dataset.monthlyVariance, d => d.year);
   endYear = d3.max(dataset.monthlyVariance, d => d.year);
   rangeOfYears = endYear - startYear;
+
   rectWidth = w / rangeOfYears;
+  xScale = w / rangeOfYears;
 
   loadPage();
 }
@@ -39,22 +47,59 @@ function loadPage() {
   //TODO passes test but doesn't show text
   svg.append('div').attr('id', 'description').text('hiiii');
 
+  const yAxisScale = d3
+    .scaleTime()
+    .domain([new Date('2014-01-01'), new Date('2014-12-31')])
+    .range([0, h]);
+
+  const yAxis = d3.axisRight(yAxisScale).tickFormat(d3.timeFormat('%B'));
+  //   const yAxis = d3.axisRight(yAxisScale).ticks(11);
+  //   const yAxis = d3.axisRight(yAxisScale);
+  //   .ticks(12)
+  //   .tickFormat(d3.timeFormat('%B'));
+
+  const xAxisScale = d3
+    .scaleTime()
+    .domain([new Date(startYear + ''), new Date(endYear + '')])
+    .range([0, w]);
+
+  const xAxis = d3.axisTop().scale(xAxisScale);
+
+  svg
+    .append('g')
+    .attr('id', 'x-axis')
+    .attr('transform', `translate(0, ${h})`)
+    .call(xAxis);
+
+  svg
+    .append('g')
+    .attr('id', 'y-axis')
+    .attr('transform', `translate(0, 0)`)
+    .call(yAxis);
+
   svg
     .selectAll('rect')
     .data(dataset.monthlyVariance)
     .enter()
     .append('rect')
     .attr('class', 'cell')
-    .attr('x', 100)
-    .attr('y', (d, i) => {
-      //   console.log(d, i);
-      //TODO
-      //put this in right spot based on d.Month-1 in regards to height
-      return 100 + i * (size + 5);
-    })
     .attr('width', rectWidth)
     .attr('height', rectHeight)
-    .style('fill', 'red')
+    .attr('x', d => {
+      //   console.log('hi');
+      //   console.log(d.year - startYear);
+      const num = (d.year - startYear) * xScale;
+      console.log(num);
+      return num;
+    })
+    // .attr('x', 100)
+    .attr('y', (d, i) => {
+      console.log(d, i);
+      //TODO
+      //put this in right spot based on d.Month-1 in regards to height
+      return h - (d.Month - 1) * yScale;
+    })
+    .style('fill', 'blue')
     .attr('data-month', (d, i) => {
       return d.month - 1;
     })
@@ -66,18 +111,6 @@ function loadPage() {
       return d.variance + baseTemperature;
     });
   // .style('fill', d => color(d));
-
-  const xAxisScale = d3
-    .scaleTime()
-    .domain([new Date(startYear + ''), new Date(endYear + '')])
-    .range([0, w]);
-
-  const xAxis = d3.axisTop().scale(xAxisScale);
-  svg
-    .append('g')
-    .attr('id', 'x-axis')
-    .attr('transform', `translate(0, ${h})`)
-    .call(xAxis);
 }
 
 getData();
